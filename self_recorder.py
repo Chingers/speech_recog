8aa0d427f7e18bfa21bed6135f580132072f4c96
Ramonito C1 <mcopreros@icloud.com>
2020-04-12 16:42:56 -0400

    C1: Refactor and git test

--- self_recorder.py
+++ self_recorder.py
@@ -2,13 +2,13 @@ import sounddevice as sd
 import soundfile as sf
 from os.path import join
 
-samplerate = 16000
+sample_rate = 16000
 duration = 1 # seconds
 folder = "my_sounds"
 filename = input("Enter filename: ")
 input("Enter to start ")
-mydata = sd.rec(int(samplerate * duration), samplerate=samplerate,
-    channels=1, blocking=True)
+my_data = sd.rec(int(sample_rate * duration), samplerate=sample_rate,
+                channels=1, blocking=True)
 print("Done recording")
 sd.wait()
-sf.write(join(folder, filename + ".wav"), mydata, samplerate)
+sf.write(join(folder, filename + ".wav"), my_data, sample_rate)
