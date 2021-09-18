6d50e71c0d8f8d330c3adbfdb7a9cfbb5ea75017
Ramonito Copreros <41483677+RCopJr@users.noreply.github.com>
2021-09-18 17:35:12 -0400

    Update feature_extraction.py

--- feature_extraction.py
+++ feature_extraction.py
@@ -17,21 +17,3 @@ def wav_to_mfcc(audio, n_mfcc=20, max_len=s.img_width):
         mfcc = mfcc[:, :max_len]
 
     return mfcc
-
-# train_audio_path = r'C:\Users\Ramonito\Datasets\speech_commands_train_audio'
-# new_sampling_rate = 8000
-# labels = ["left", "right", "go", "stop"]
-# counter = 1
-# for label in labels:
-#
-#     print(label)
-#     wave_files = [f for f in os.listdir(train_audio_path + '\\' + label) if f.endswith('.wav')]
-#     print(len(wave_files))
-#
-#     for wave_file in wave_files:
-#         if counter == 1:
-#             samples, sampling_rate = librosa.load(os.path.join(train_audio_path, label, wave_file), sr=16000)
-#             samples = librosa.resample(samples, sampling_rate, new_sampling_rate)
-#             mfcc = wav2mfcc(os.path.join(train_audio_path, label, wave_file))
-#             print("")
-
